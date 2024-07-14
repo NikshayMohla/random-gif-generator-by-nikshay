ed74f1053a33e83168778333ecea00b168295cda
NikshayMohla <78416149+NikshayMohla@users.noreply.github.com>
2024-07-14 22:32:59 +0530

    Search Functionality

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Spinner from './Spinner';
@@ -10,7 +9,7 @@ const Tag = () => {
   const [gif, setGif] = useState("");
   async function fetchData() {
     setLoading(true);
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
     const { data } = await axios.get(url);
     const imageSource = data.data.images.downsized_large.url;
     // console.log(imageSource);
@@ -30,7 +29,7 @@ const Tag = () => {
   const [loading, setLoading] = useState(false)
   return (
     <div className='w-1/2 bg-blue-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]'>
-      <h1 className='text-3xl mt-[15px] mb-[20px] underline uppercase font-bold'>Tagged Random GIFs</h1>
+      <h1 className='text-3xl mt-[15px] mb-[20px] underline uppercase font-bold'>Random {tag} GIFs</h1>
       {loading ? (<Spinner />) : <img src={gif} width='450' />}
 
       <input className='w-10/12 text-lg py-2 rounded-lg mb-[3px] text-center' onChange={changeHandler} value={tag}></input>
